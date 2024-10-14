5602bc931b5834add45c0308cee89aeae6e72afa
Gavin-Inman-Student <gain6ab@student.oaisd.org>
2024-10-14 18:09:40 +0000

    Completed a Switch Statment Stating What Number It Is That You Have Imputed

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Clear();
+
+Console.WriteLine("Enter a Number 1-10");
+string num = Console.ReadLine();
+int number = int.Parse(num);
+
+switch (number){
+case 1:
+    Console.WriteLine("Your Number Is One");
+    break;
+case 2:
+    Console.WriteLine("Your Number Is Two");
+    break;
+case 3:
+    Console.WriteLine("Your Number Is Three");
+    break;
+case 4:
+    Console.WriteLine("Your Number Is Four");
+    break;
+case 5:
+    Console.WriteLine("Your Number Is Five");
+    break;
+case 6:
+    Console.WriteLine("Your Number Is Six");
+    break;
+case 7:
+    Console.WriteLine("Your Number Is Seven");
+    break;
+case 8:
+    Console.WriteLine("Your Number Is Eight");
+    break;
+case 9:
+    Console.WriteLine("Your Number Is Nine");
+    break;
+case 10:
+    Console.WriteLine("Your Number Is Ten");
+    break;
+}
